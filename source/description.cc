fefb6494bd7ae2453cdfbe0c0704a48ec33c04ef
Logan Harbour <loganhharbour@gmail.com>
2019-03-08 11:18:20 -0600

    Add isotropic/perpendicular indicent bc parameters

--- source/description.cc
+++ source/description.cc
@@ -1,6 +1,7 @@
 #include "description.h"
 #include "material.h"
 
+#include <algorithm>
 #include <deal.II/base/exceptions.h>
 
 namespace SNProblem
@@ -9,43 +10,77 @@ using namespace dealii;
 
 Description::Description() : ParameterAcceptor("Description")
 {
-  input_material_id = {0};
-  add_parameter("material_id", input_material_id);
+  // Initialize default material properties
+  material_ids = {0};
+  material_sigma_t = {100.0};
+  material_sigma_s = {0.0};
+  material_src = {1.0};
+  // Initialize default boundary conditions
+  perpendicular_boundary_ids = {};
+  perpendicular_boundary_fluxes = {};
+  isotropic_boundary_ids = {};
+  isotropic_boundary_fluxes = {};
 
-  input_material_sigma_t = {100.0};
-  add_parameter("material_sigma_t", input_material_sigma_t);
-
-  input_material_sigma_s = {0.0};
-  add_parameter("material_sigma_s", input_material_sigma_s);
-
-  input_material_src = {1.0};
-  add_parameter("material_src", input_material_src);
+  // Add material parameters
+  add_parameter("material_ids", material_ids);
+  add_parameter("material_sigma_t", material_sigma_t);
+  add_parameter("material_sigma_s", material_sigma_s);
+  add_parameter("material_src", material_src);
+  // Add boundary parameters
+  add_parameter("perpendicular_boundary_ids", perpendicular_boundary_ids);
+  add_parameter("perpendicular_boundary_fluxes", perpendicular_boundary_fluxes);
+  add_parameter("isotropic_boundary_ids", isotropic_boundary_ids);
+  add_parameter("isotropic_boundary_fluxes", isotropic_boundary_fluxes);
 }
 
 void
 Description::setup()
 {
   setup_materials();
+  setup_boundary_conditions();
 }
 
 void
 Description::setup_materials()
 {
   // Make sure all material vectors are the same size
-  const auto & double_inputs = {input_material_sigma_t, input_material_sigma_s, input_material_src};
-  for (const auto input : double_inputs)
-    if (input.size() != input_material_id.size())
+  const auto & double_inputs = {material_sigma_t, material_sigma_s, material_src};
+  for (const auto & input : double_inputs)
+    if (input.size() != material_ids.size())
       throw ExcMessage("Material input size mismatch");
 
   // Create a Material for each id
-  for (unsigned int i = 0; i < input_material_id.size(); ++i)
+  for (unsigned int i = 0; i < material_ids.size(); ++i)
   {
-    if (materials.find(input_material_id[i]) != materials.end())
+    if (materials.find(material_ids[i]) != materials.end())
       throw ExcMessage("Material id already exists in material map");
-    materials.emplace(input_material_id[i],
-                      Material(input_material_sigma_t[i],
-                               input_material_sigma_s[i],
-                               input_material_src[i]));
+    materials.emplace(material_ids[i],
+                      Material(material_sigma_t[i], material_sigma_s[i], material_src[i]));
   }
 }
+
+void
+Description::setup_boundary_conditions()
+{
+  // Fill perpendicular boundary conditions
+  if (perpendicular_boundary_ids.size() != perpendicular_boundary_fluxes.size())
+    throw ExcMessage("Perpendicular boundary inputs are not of the same size");
+  for (unsigned int i = 0; i < perpendicular_boundary_ids.size(); ++i)
+  {
+    if (perpendicular_bcs.find(perpendicular_boundary_ids[i]) != perpendicular_bcs.end())
+      throw ExcMessage("Duplicate perpendicular boundary ids provided");
+    perpendicular_bcs.emplace(perpendicular_boundary_ids[i], perpendicular_boundary_fluxes[i]);
+  }
+
+  // Fill isotropic boundary conditions
+  if (isotropic_boundary_ids.size() != isotropic_boundary_fluxes.size())
+    throw ExcMessage("isotropic boundary inputs are not of the same size");
+  for (unsigned int i = 0; i < isotropic_boundary_ids.size(); ++i)
+  {
+    if (isotropic_bcs.find(isotropic_boundary_ids[i]) != isotropic_bcs.end())
+      throw ExcMessage("Duplicate isotropic boundary ids provided");
+    isotropic_bcs.emplace(isotropic_boundary_ids[i], isotropic_boundary_fluxes[i]);
+  }
+}
+
 } // namespace SNProblem
--- source/description.h
+++ source/description.h
@@ -20,13 +20,25 @@ public:
 
 private:
   void setup_materials();
+  void setup_boundary_conditions();
 
   std::map<const unsigned int, const Material> materials;
 
-  std::vector<unsigned int> input_material_id;
-  std::vector<double> input_material_sigma_t;
-  std::vector<double> input_material_sigma_s;
-  std::vector<double> input_material_src;
+  // Boundary conditions
+  std::map<const unsigned int, const double> perpendicular_bcs;
+  std::map<const unsigned int, const double> isotropic_bcs;
+
+  std::vector<unsigned int> material_ids;
+  std::vector<double> material_sigma_t;
+  std::vector<double> material_sigma_s;
+  std::vector<double> material_src;
+
+  // Input perpendicular incident flux boundary conditions
+  std::vector<unsigned int> perpendicular_boundary_ids;
+  std::vector<double> perpendicular_boundary_fluxes;
+  // Input isotropic incident flux boundary conditions
+  std::vector<unsigned int> isotropic_boundary_ids;
+  std::vector<double> isotropic_boundary_fluxes;
 };
 } // namespace SNProblem
 
